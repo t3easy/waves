73e1f09416c07619d091c5ff1498b3b5351da998
Jan Kiesewetter <jan@t3easy.de>
2017-06-06 19:50:43 +0200

    [TASK] Add KillPhp task and metafiles

---
+++ src/Task/KillPhpTask.php
@@ -0,0 +1,29 @@
+<?php
+namespace T3easy\Waves;
+
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Task;
+use TYPO3\Surf\Domain\Service\ShellCommandServiceAwareInterface;
+use TYPO3\Surf\Domain\Service\ShellCommandServiceAwareTrait;
+
+class KillPhpTask extends Task implements ShellCommandServiceAwareInterface
+{
+    use ShellCommandServiceAwareTrait;
+
+    /**
+     * Executes this action
+     *
+     * @param \TYPO3\Surf\Domain\Model\Node $node
+     * @param \TYPO3\Surf\Domain\Model\Application $application
+     * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+     * @param array $options
+     * @return void
+     */
+    public function execute(Node $node, Application $application, Deployment $deployment, array $options = [])
+    {
+        $command = 'killall -q php-cgi';
+        $this->shell->executeOrSimulate($command, $node, $deployment);
+    }
+}
